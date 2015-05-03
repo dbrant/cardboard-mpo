554d8b70ed330116f70480101f2bc0988431756f
dbrant <dbrant@wikimedia.org>
2015-05-02 21:21:30 -0400

    First commit.

---
+++ CardboardMpo/src/main/java/com/dmitrybrant/android/cardboardmpo/MainActivity.java
@@ -0,0 +1,94 @@
+
+package com.dmitrybrant.android.cardboardmpo;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer {
+  private static final String TAG = "MainActivity";
+
+  private Vibrator vibrator;
+
+  /**
+   * Sets the view to our CardboardView and initializes the transformation matrices we will use
+   * to render our scene.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.common_ui);
+    CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+    cardboardView.setRenderer(this);
+    setCardboardView(cardboardView);
+
+    vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+  }
+
+  @Override
+  public void onRendererShutdown() {
+    Log.i(TAG, "onRendererShutdown");
+  }
+
+  @Override
+  public void onSurfaceChanged(int width, int height) {
+    Log.i(TAG, "onSurfaceChanged");
+  }
+
+  /**
+   * Creates the buffers we use to store information about the 3D world.
+   *
+   * <p>OpenGL doesn't use Java arrays, but rather needs data in a format it can understand.
+   * Hence we use ByteBuffers.
+   *
+   * @param config The EGL configuration used when creating the surface.
+   */
+  @Override
+  public void onSurfaceCreated(EGLConfig config) {
+  }
+
+  /**
+   * Prepares OpenGL ES before we draw a frame.
+   *
+   * @param headTransform The head transformation in the new frame.
+   */
+  @Override
+  public void onNewFrame(HeadTransform headTransform) {
+  }
+
+  /**
+   * Draws a frame for an eye.
+   *
+   * @param eye The eye to render. Includes all required transformations.
+   */
+  @Override
+  public void onDrawEye(Eye eye) {
+  }
+
+  @Override
+  public void onFinishFrame(Viewport viewport) {
+  }
+
+  /**
+   * Called when the Cardboard trigger is pulled.
+   */
+  @Override
+  public void onCardboardTrigger() {
+    Log.i(TAG, "onCardboardTrigger");
+
+    // Always give user feedback.
+    vibrator.vibrate(50);
+  }
+
+}
