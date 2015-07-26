f43ea95e7469214e14f3e83e7a36f062865440bc
dbrant <dbrant@wikimedia.org>
2015-07-25 21:52:43 -0400

    Added more mount points to search for files.

--- CardboardMpo/src/main/java/com/dmitrybrant/android/cardboardmpo/MainActivity.java
+++ CardboardMpo/src/main/java/com/dmitrybrant/android/cardboardmpo/MainActivity.java
@@ -44,239 +44,246 @@ import java.util.Locale;
 import javax.microedition.khronos.egl.EGLConfig;
 
 public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer {
-  private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
 
-  private List<File> mpoFileList = new ArrayList<>();
-  private int currentFileIndex = 0;
+    private List<File> mpoFileList = new ArrayList<>();
+    private int currentFileIndex = 0;
 
-  private Vibrator vibrator;
-  private AlphaAnimation fadeInAnim;
+    private Vibrator vibrator;
+    private AlphaAnimation fadeInAnim;
 
-  private ImageView imageLeft;
-  private ImageView imageRight;
-  private ProgressBar progressLeft;
-  private ProgressBar progressRight;
-  private TextView statusLeft;
-  private TextView statusRight;
+    private ImageView imageLeft;
+    private ImageView imageRight;
+    private ProgressBar progressLeft;
+    private ProgressBar progressRight;
+    private TextView statusLeft;
+    private TextView statusRight;
 
-  private Bitmap bmpLeft;
-  private Bitmap bmpRight;
-  // final field for synchronizing access to the bitmaps above
-  private final Boolean bmpLock = false;
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    setContentView(R.layout.common_ui);
-    CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
-    cardboardView.setRenderer(this);
-    setCardboardView(cardboardView);
-
-    vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-    imageLeft = (ImageView) findViewById(R.id.image_left);
-    imageRight = (ImageView) findViewById(R.id.image_right);
-    progressLeft = (ProgressBar) findViewById(R.id.progress_left);
-    progressRight = (ProgressBar) findViewById(R.id.progress_right);
-    statusLeft = (TextView) findViewById(R.id.status_text_left);
-    statusRight = (TextView) findViewById(R.id.status_text_right);
-
-    fadeInAnim = new AlphaAnimation(0.0f, 1.0f);
-    fadeInAnim.setDuration(500);
-
-    setProgress(true);
-    setStatus(true, getString(R.string.status_finding_files));
-
-    // kick off our task to find all MPOs, which will in turn kick off showing the first one.
-    new MpoFindTask().execute((Void)null);
-  }
-
-  private void setProgress(boolean enabled) {
-    progressLeft.setVisibility(enabled ? View.VISIBLE : View.GONE);
-    progressRight.setVisibility(enabled ? View.VISIBLE : View.GONE);
-  }
-
-  private void setStatus(boolean visible, String status) {
-    statusLeft.setVisibility(visible ? View.VISIBLE : View.GONE);
-    statusRight.setVisibility(visible ? View.VISIBLE : View.GONE);
-    statusLeft.setText(status);
-    statusRight.setText(status);
-  }
-
-  @Override
-  public void onRendererShutdown() {
-  }
-
-  @Override
-  public void onSurfaceChanged(int width, int height) {
-  }
-
-  @Override
-  public void onSurfaceCreated(EGLConfig config) {
-  }
-
-  @Override
-  public void onNewFrame(HeadTransform headTransform) {
-    // TODO: shift the image based on the head transform.
-  }
-
-  @Override
-  public void onDrawEye(Eye eye) {
-  }
-
-  @Override
-  public void onFinishFrame(Viewport viewport) {
-  }
-
-  /**
-   * Called when the Cardboard trigger is pulled.
-   * This is the only thing we'll really "use" from the CardboardView...
-   */
-  @Override
-  public void onCardboardTrigger() {
-    Log.d(TAG, "onCardboardTrigger");
-
-    currentFileIndex++;
-    loadNextMpo();
-
-    // Always give user feedback.
-    vibrator.vibrate(50);
-  }
-
-  /**
-   * Task that finds all MPO files in external storage.
-   * Returns a (flat) list of File objects.
-   */
-  private class MpoFindTask extends AsyncTask<Void, Integer, List<File>> {
-
-    private List<File> getMpoFiles(File parentDir, int level) {
-      ArrayList<File> inFiles = new ArrayList<>();
-      if (parentDir == null || level > 3) {
-        return inFiles;
-      }
-      File[] files = parentDir.listFiles();
-      if (files == null) {
-        return inFiles;
-      }
-      for (File file : files) {
-        if (file.isDirectory()) {
-          inFiles.addAll(getMpoFiles(file, level + 1));
-        } else {
-          if (file.getName().toLowerCase(Locale.ENGLISH).endsWith(".mpo")) {
-            inFiles.add(file);
-            Log.d(TAG, "Found MPO: " + file.getAbsolutePath());
-          }
-        }
-      }
-      return inFiles;
-    }
-
-    protected List<File> doInBackground(Void... dummy) {
-      List<File> mpoFiles = new ArrayList<>();
-      if (Environment.getExternalStorageDirectory() != null) {
-        mpoFiles.addAll(getMpoFiles(Environment.getExternalStorageDirectory(), 0));
-      } else {
-        mpoFiles.addAll(getMpoFiles(new File("/mnt/sdcard"), 0));
-      }
-      // TODO: find better way of getting external SD card directory?
-      mpoFiles.addAll(getMpoFiles(new File("/mnt/extSdCard"), 0));
-      return mpoFiles;
-    }
-
-    protected void onProgressUpdate(Integer... progress) {
-    }
-
-    protected void onPostExecute(List<File> results) {
-      mpoFileList.clear();
-      setProgress(false);
-      if (results.size() == 0) {
-        setStatus(true, getString(R.string.status_error_not_found));
-        return;
-      }
-      mpoFileList.addAll(results);
-      currentFileIndex = 0;
-      setStatus(false, "");
-      loadNextMpo();
-    }
-  }
-
-  private class MainLoadTask extends MpoUtils.MpoLoadTask {
+    private Bitmap bmpLeft;
+    private Bitmap bmpRight;
+    // final field for synchronizing access to the bitmaps above
+    private final Boolean bmpLock = false;
 
     @Override
-    protected List<Long> doInBackground(File... file) {
-      List<Long> results = super.doInBackground(file);
-      try {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.common_ui);
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(this);
+        setCardboardView(cardboardView);
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        imageLeft = (ImageView) findViewById(R.id.image_left);
+        imageRight = (ImageView) findViewById(R.id.image_right);
+        progressLeft = (ProgressBar) findViewById(R.id.progress_left);
+        progressRight = (ProgressBar) findViewById(R.id.progress_right);
+        statusLeft = (TextView) findViewById(R.id.status_text_left);
+        statusRight = (TextView) findViewById(R.id.status_text_right);
+
+        fadeInAnim = new AlphaAnimation(0.0f, 1.0f);
+        fadeInAnim.setDuration(500);
+
+        setProgress(true);
+        setStatus(true, getString(R.string.status_finding_files));
+
+        // kick off our task to find all MPOs, which will in turn kick off showing the first one.
+        new MpoFindTask().execute((Void) null);
+    }
+
+    private void setProgress(boolean enabled) {
+        progressLeft.setVisibility(enabled ? View.VISIBLE : View.GONE);
+        progressRight.setVisibility(enabled ? View.VISIBLE : View.GONE);
+    }
+
+    private void setStatus(boolean visible, String status) {
+        statusLeft.setVisibility(visible ? View.VISIBLE : View.GONE);
+        statusRight.setVisibility(visible ? View.VISIBLE : View.GONE);
+        statusLeft.setText(status);
+        statusRight.setText(status);
+    }
+
+    @Override
+    public void onRendererShutdown() {
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig config) {
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+        // TODO: shift the image based on the head transform.
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+    }
+
+    /**
+     * Called when the Cardboard trigger is pulled.
+     * This is the only thing we'll really "use" from the CardboardView...
+     */
+    @Override
+    public void onCardboardTrigger() {
+        Log.d(TAG, "onCardboardTrigger");
+
+        currentFileIndex++;
+        loadNextMpo();
+
+        // Always give user feedback.
+        vibrator.vibrate(50);
+    }
+
+    /**
+     * Task that finds all MPO files in external storage.
+     * Returns a (flat) list of File objects.
+     */
+    private class MpoFindTask extends AsyncTask<Void, Integer, List<File>> {
+
+        private List<File> getMpoFiles(File parentDir, int level) {
+            ArrayList<File> inFiles = new ArrayList<>();
+            if (parentDir == null || level > 2) {
+                return inFiles;
+            }
+            File[] files = parentDir.listFiles();
+            if (files == null) {
+                return inFiles;
+            }
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    inFiles.addAll(getMpoFiles(file, level + 1));
+                } else {
+                    if (file.getName().toLowerCase(Locale.ENGLISH).endsWith(".mpo")) {
+                        inFiles.add(file);
+                        Log.d(TAG, "Found MPO: " + file.getAbsolutePath());
+                    }
+                }
+            }
+            return inFiles;
+        }
+
+        protected List<File> doInBackground(Void... dummy) {
+            List<File> mpoFiles = new ArrayList<>();
+            if (Environment.getExternalStorageDirectory() != null) {
+                mpoFiles.addAll(getMpoFiles(Environment.getExternalStorageDirectory(), 0));
+            } else if (new File("/sdcard").exists()) {
+                mpoFiles.addAll(getMpoFiles(new File("/sdcard"), 0));
+            } else if (new File("/mnt/sdcard").exists()) {
+                mpoFiles.addAll(getMpoFiles(new File("/mnt/sdcard"), 0));
+            }
+            // TODO: find better way of getting external SD card directory?
+            if (new File("/mnt/extSdCard").exists()) {
+                mpoFiles.addAll(getMpoFiles(new File("/mnt/extSdCard"), 0));
+            }
+            if (new File("/mnt/UsbDriveA").exists()) {
+                mpoFiles.addAll(getMpoFiles(new File("/mnt/UsbDriveA"), 0));
+            }
+            return mpoFiles;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        protected void onPostExecute(List<File> results) {
+            mpoFileList.clear();
+            setProgress(false);
+            if (results.size() == 0) {
+                setStatus(true, getString(R.string.status_error_not_found));
+                return;
+            }
+            mpoFileList.addAll(results);
+            currentFileIndex = 0;
+            setStatus(false, "");
+            loadNextMpo();
+        }
+    }
+
+    private class MainLoadTask extends MpoUtils.MpoLoadTask {
+
+        @Override
+        protected List<Long> doInBackground(File... file) {
+            List<Long> results = super.doInBackground(file);
+            try {
+                synchronized (bmpLock) {
+                    if (results.size() == 2) {
+                        // this is the most common type of MPO, which is left-eye / right-eye
+                        Log.d(TAG, "Found 2 JPGs, so loading 0/1...");
+                        bmpLeft = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(0), imageLeft.getWidth(), imageLeft.getHeight());
+                        bmpRight = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(1), imageRight.getWidth(), imageRight.getHeight());
+                    } else if (results.size() == 4) {
+                        // I've seen this type in the wild, as well, which seems to be
+                        // left-eye-hi-res / left-eye-lo-res / right-eye-hi-res / right-eye-lo-res
+                        Log.d(TAG, "Found 4 JPGs, so loading 0/2...");
+                        bmpLeft = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(0), imageLeft.getWidth(), imageLeft.getHeight());
+                        bmpRight = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(2), imageRight.getWidth(), imageRight.getHeight());
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Error while reading file.", e);
+            }
+            return results;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        @Override
+        protected void onPostExecute(List<Long> results) {
+            synchronized (bmpLock) {
+                if (bmpLeft == null || bmpRight == null) {
+                    setStatus(true, getString(R.string.status_error_load));
+                } else {
+                    imageLeft.setImageBitmap(bmpLeft);
+                    imageRight.setImageBitmap(bmpRight);
+                    imageLeft.startAnimation(fadeInAnim);
+                    imageRight.startAnimation(fadeInAnim);
+                    setStatus(false, "");
+                }
+            }
+            setProgress(false);
+        }
+    }
+
+    /**
+     * Load the next MPO file in our sequence. Wrap to the beginning if we're at the end.
+     */
+    private void loadNextMpo() {
+        if (mpoFileList.size() == 0) {
+            return;
+        }
+        if (currentFileIndex >= mpoFileList.size()) {
+            currentFileIndex = 0;
+        }
+        setProgress(true);
+        setStatus(true, String.format(getString(R.string.status_loading_file), mpoFileList.get(currentFileIndex).getName()));
+        imageLeft.setImageDrawable(null);
+        imageRight.setImageDrawable(null);
         synchronized (bmpLock) {
-          if (results.size() == 2) {
-            // this is the most common type of MPO, which is left-eye / right-eye
-            Log.d(TAG, "Found 2 JPGs, so loading 0/1...");
-            bmpLeft = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(0), imageLeft.getWidth(), imageLeft.getHeight());
-            bmpRight = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(1), imageRight.getWidth(), imageRight.getHeight());
-          } else if (results.size() == 4) {
-            // I've seen this type in the wild, as well, which seems to be
-            // left-eye-hi-res / left-eye-lo-res / right-eye-hi-res / right-eye-lo-res
-            Log.d(TAG, "Found 4 JPGs, so loading 0/2...");
-            bmpLeft = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(0), imageLeft.getWidth(), imageLeft.getHeight());
-            bmpRight = MpoUtils.loadMpoBitmapFromFile(mpoFile, results.get(2), imageRight.getWidth(), imageRight.getHeight());
-          }
+            cleanupBitmap(bmpLeft);
+            cleanupBitmap(bmpRight);
         }
-      } catch (IOException e) {
-        Log.e(TAG, "Error while reading file.", e);
-      }
-      return results;
+        new MainLoadTask().execute(mpoFileList.get(currentFileIndex));
     }
 
-    @Override
-    protected void onProgressUpdate(Integer... progress) {
-    }
-
-    @Override
-    protected void onPostExecute(List<Long> results) {
-      synchronized (bmpLock) {
-        if (bmpLeft == null || bmpRight == null) {
-          setStatus(true, getString(R.string.status_error_load));
-        } else {
-          imageLeft.setImageBitmap(bmpLeft);
-          imageRight.setImageBitmap(bmpRight);
-          imageLeft.startAnimation(fadeInAnim);
-          imageRight.startAnimation(fadeInAnim);
-          setStatus(false, "");
+    private void cleanupBitmap(Bitmap bmp) {
+        if (bmp != null) {
+            try {
+                bmp.recycle();
+            } catch (Exception e) {
+                Log.e(TAG, "Error while cleaning up bitmap.", e);
+            }
         }
-      }
-      setProgress(false);
     }
-  }
-
-  /**
-   * Load the next MPO file in our sequence. Wrap to the beginning if we're at the end.
-   */
-  private void loadNextMpo() {
-    if (mpoFileList.size() == 0) {
-      return;
-    }
-    if (currentFileIndex >= mpoFileList.size()) {
-      currentFileIndex = 0;
-    }
-    setProgress(true);
-    setStatus(true, String.format(getString(R.string.status_loading_file), mpoFileList.get(currentFileIndex).getName()));
-    imageLeft.setImageDrawable(null);
-    imageRight.setImageDrawable(null);
-    synchronized (bmpLock) {
-      cleanupBitmap(bmpLeft);
-      cleanupBitmap(bmpRight);
-    }
-    new MainLoadTask().execute(mpoFileList.get(currentFileIndex));
-  }
-
-  private void cleanupBitmap(Bitmap bmp) {
-    if (bmp != null) {
-      try {
-        bmp.recycle();
-      } catch (Exception e) {
-        Log.e(TAG, "Error while cleaning up bitmap.", e);
-      }
-    }
-  }
 
 }
--- CardboardMpo/src/main/java/com/dmitrybrant/android/cardboardmpo/MpoUtils.java
+++ CardboardMpo/src/main/java/com/dmitrybrant/android/cardboardmpo/MpoUtils.java
@@ -28,124 +28,125 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MpoUtils {
-  private static final String TAG = "MpoUtils";
+    private static final String TAG = "MpoUtils";
 
-  /**
-   * Task that facilitates loading of MPO files.
-   * For a given MPO file, it returns a list of offsets that point to each individual
-   * JPG chunk that the MPO contains.
-   */
-  public static class MpoLoadTask extends AsyncTask<File, Integer, List<Long>> {
-    protected File mpoFile;
+    /**
+     * Task that facilitates loading of MPO files.
+     * For a given MPO file, it returns a list of offsets that point to each individual
+     * JPG chunk that the MPO contains.
+     */
+    public static class MpoLoadTask extends AsyncTask<File, Integer, List<Long>> {
+        protected File mpoFile;
 
-    protected List<Long> doInBackground(File... file) {
-      /**
-       * An MPO file is simply multiple JPG files concatenated together.
-       * We can detect their locations by simply looking for the beginning markers of a
-       * JPG image. So that's what we'll do.
-       */
-      final int chunkLength = 4096;
-      final byte[] sig1 = new byte[] { (byte)0xff, (byte)0xd8, (byte)0xff, (byte)0xe0 };
-      final byte[] sig2 = new byte[] { (byte)0xff, (byte)0xd8, (byte)0xff, (byte)0xe1 };
-      mpoFile = file[0];
-      List<Long> mpoOffsets = new ArrayList<>();
+        protected List<Long> doInBackground(File... file) {
+            /**
+             * An MPO file is simply multiple JPG files concatenated together.
+             * We can detect their locations by simply looking for the beginning markers of a
+             * JPG image. So that's what we'll do.
+             */
+            final int chunkLength = 4096;
+            final byte[] sig1 = new byte[]{(byte) 0xff, (byte) 0xd8, (byte) 0xff, (byte) 0xe0};
+            final byte[] sig2 = new byte[]{(byte) 0xff, (byte) 0xd8, (byte) 0xff, (byte) 0xe1};
+            mpoFile = file[0];
+            List<Long> mpoOffsets = new ArrayList<>();
 
-      FileInputStream fs = null;
-      try {
-        fs = new FileInputStream(mpoFile);
-        byte[] tempBytes = new byte[chunkLength];
-        long currentOffset = 0;
+            FileInputStream fs = null;
+            try {
+                fs = new FileInputStream(mpoFile);
+                byte[] tempBytes = new byte[chunkLength];
+                long currentOffset = 0;
 
-        Log.d(TAG, "Processing file: " + mpoFile.getName());
-        while (true) {
-          if (fs.read(tempBytes, 0, chunkLength) <= 0) {
-            break;
-          }
-          int sigOffset = searchBytes(tempBytes, sig1, 0, chunkLength);
-          if (sigOffset == -1) {
-            sigOffset = searchBytes(tempBytes, sig2, 0, chunkLength);
-          }
-          if (sigOffset >= 0) {
-            // it's a new image
-            Log.d(TAG, "Found new JPG at offset " + (currentOffset + sigOffset));
-            mpoOffsets.add(currentOffset + sigOffset);
-          }
-          currentOffset += chunkLength;
+                Log.d(TAG, "Processing file: " + mpoFile.getName());
+                while (true) {
+                    if (fs.read(tempBytes, 0, chunkLength) <= 0) {
+                        break;
+                    }
+                    int sigOffset = searchBytes(tempBytes, sig1, 0, chunkLength);
+                    if (sigOffset == -1) {
+                        sigOffset = searchBytes(tempBytes, sig2, 0, chunkLength);
+                    }
+                    if (sigOffset >= 0) {
+                        // it's a new image
+                        Log.d(TAG, "Found new JPG at offset " + (currentOffset + sigOffset));
+                        mpoOffsets.add(currentOffset + sigOffset);
+                    }
+                    currentOffset += chunkLength;
+                }
+
+            } catch (IOException e) {
+                Log.e(TAG, "Error while reading file.", e);
+            }
+            if (fs != null) {
+                try {
+                    fs.close();
+                } catch (IOException e) {
+                    // don't worry
+                }
+            }
+            return mpoOffsets;
         }
 
-      } catch (IOException e) {
-        Log.e(TAG, "Error while reading file.", e);
-      }
-      if (fs != null) {
-        try {
-          fs.close();
-        } catch (IOException e) {
-          // don't worry
+        protected void onProgressUpdate(Integer... progress) {
         }
-      }
-      return mpoOffsets;
-    }
 
-    protected void onProgressUpdate(Integer... progress) {
-    }
-
-    protected void onPostExecute(List<Long> results) {
-    }
-  }
-
-  public static Bitmap loadMpoBitmapFromFile(File file, long offset, int maxWidth, int maxHeight) throws IOException {
-    // First, decode the width and height of the image, so that we know how much to scale
-    // it down when loading it into our ImageView (so we don't need to do a huge allocation).
-    BitmapFactory.Options opts = new BitmapFactory.Options();
-    FileInputStream fs = new FileInputStream(file);
-    fs.skip(offset);
-    opts.inJustDecodeBounds = true;
-    BitmapFactory.decodeStream(fs, null, opts);
-    fs.close();
-    int scale = 1;
-    if (opts.outHeight > maxHeight || opts.outWidth > maxWidth) {
-      scale = (int) Math.pow(2, (int) Math.round(Math.log(maxWidth / (double) Math.max(opts.outHeight, opts.outWidth)) / Math.log(0.5)));
-    }
-    if ((opts.outHeight <= 0) || (opts.outWidth <= 0)) {
-      return null;
-    }
-    // Decode the image for real, but now with a sampleSize.
-    // We have to reopen the file stream, and re-skip to the correct offset, since
-    // FileInputStream doesn't support reset().
-    fs = new FileInputStream(file);
-    fs.skip(offset);
-    BitmapFactory.Options opts2 = new BitmapFactory.Options();
-    opts2.inSampleSize = scale;
-    Bitmap bmp = BitmapFactory.decodeStream(fs, null, opts2);
-    fs.close();
-    return bmp;
-  }
-
-  /**
-   * Search a byte array for a specific sequence of bytes.
-   * @param bytesToSearch Byte array to search.
-   * @param matchBytes Sequence of bytes to find in the array.
-   * @param startIndex Offset within the array from which to start searching.
-   * @param count Number of bytes within the array to search.
-   * @return Byte offset within the array of the specified sequence, or -1 if it was not found.
-   */
-  public static int searchBytes(byte[] bytesToSearch, byte[] matchBytes, int startIndex, int count) {
-    int ret = -1, max = count - matchBytes.length + 1;
-    boolean found;
-    for (int i = startIndex; i < max; i++) {
-      found = true;
-      for (int j = 0; j < matchBytes.length; j++) {
-        if (bytesToSearch[i + j] != matchBytes[j]) {
-          found = false;
-          break;
+        protected void onPostExecute(List<Long> results) {
         }
-      }
-      if (found) {
-        ret = i;
-        break;
-      }
     }
-    return ret;
-  }
+
+    public static Bitmap loadMpoBitmapFromFile(File file, long offset, int maxWidth, int maxHeight) throws IOException {
+        // First, decode the width and height of the image, so that we know how much to scale
+        // it down when loading it into our ImageView (so we don't need to do a huge allocation).
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        FileInputStream fs = new FileInputStream(file);
+        fs.skip(offset);
+        opts.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(fs, null, opts);
+        fs.close();
+        int scale = 1;
+        if (opts.outHeight > maxHeight || opts.outWidth > maxWidth) {
+            scale = (int) Math.pow(2, (int) Math.round(Math.log(maxWidth / (double) Math.max(opts.outHeight, opts.outWidth)) / Math.log(0.5)));
+        }
+        if ((opts.outHeight <= 0) || (opts.outWidth <= 0)) {
+            return null;
+        }
+        // Decode the image for real, but now with a sampleSize.
+        // We have to reopen the file stream, and re-skip to the correct offset, since
+        // FileInputStream doesn't support reset().
+        fs = new FileInputStream(file);
+        fs.skip(offset);
+        BitmapFactory.Options opts2 = new BitmapFactory.Options();
+        opts2.inSampleSize = scale;
+        Bitmap bmp = BitmapFactory.decodeStream(fs, null, opts2);
+        fs.close();
+        return bmp;
+    }
+
+    /**
+     * Search a byte array for a specific sequence of bytes.
+     *
+     * @param bytesToSearch Byte array to search.
+     * @param matchBytes    Sequence of bytes to find in the array.
+     * @param startIndex    Offset within the array from which to start searching.
+     * @param count         Number of bytes within the array to search.
+     * @return Byte offset within the array of the specified sequence, or -1 if it was not found.
+     */
+    public static int searchBytes(byte[] bytesToSearch, byte[] matchBytes, int startIndex, int count) {
+        int ret = -1, max = count - matchBytes.length + 1;
+        boolean found;
+        for (int i = startIndex; i < max; i++) {
+            found = true;
+            for (int j = 0; j < matchBytes.length; j++) {
+                if (bytesToSearch[i + j] != matchBytes[j]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                ret = i;
+                break;
+            }
+        }
+        return ret;
+    }
 
 }
